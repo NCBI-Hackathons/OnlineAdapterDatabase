b794e1cd126aa09ba5b3442eb3954adcf6f33b3c
Daniel Davis <dan@danizen.net>
2017-08-14 20:55:13 -0400

    Add support for both sqlite3 and postgresql
    - if DATABASE_URL is defined properly, then postgres will be used
    - if DATABASE_URL is not defined, then sqlite3 will be used
    - if DATABASE_URL is defined badly, B00m!

--- oadb/oadb/settings.py
+++ oadb/oadb/settings.py
@@ -75,12 +75,17 @@ WSGI_APPLICATION = 'oadb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = { 'default': dj_database_url.config() }
+    DATABASES['default']['ENGINE'] = 'django_postgrespool'
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 AUTH_USER_MODEL = 'oadb.User'
 

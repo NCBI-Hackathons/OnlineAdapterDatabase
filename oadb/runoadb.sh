df7203bfc5ff9ee975f44cd71876b2756a57d5f1
Daniel Davis <dan@danizen.net>
2017-08-16 09:40:26 -0400

    Revise `runoadb.sh` so that it works by default.

--- oadb/runoadb.sh
+++ oadb/runoadb.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-BINDADDR="*.*.*.*"
-BINDPORT=8001
 
-while getopts "a:p:" opt; do
+BINDADDR="0.0.0.0"
+BINDPORT=8001
+SETTINGS=oadb.settings.dev
+
+while getopts "a:p:s:h" opt; do
   case $opt in
-    a) BINDADDR=$PTARG ;;
+    h) 
+      echo "Usage: $0 [options]" ;
+      echo "   -h ...... help (this message)" ;
+      echo "   -a ...... bind address, defaults to 0.0.0.0 for all interfaces" ;
+      echo "   -p ...... bind port, defaults to 8001 to avoid conflict with runserver" ;
+      echo "   -s ...... Django settings module - this doesn't work yet" ;
+      exit 0 ;;
+    a) BINDADDR=$OPTARG ;;
     p) BINDPORT=$OPTARG ;;
-    \?) echo "error: invalid argument" >&2; exit 1 ;;
+    s) SETTINGS=$OPTARG ;;
+    \?) echo "error: invalid argument - use -h for usage" >&2; exit 1 ;;
   esac
 done
 
 shift $((OPTIND-1))
 
-gunicorn -w 4 oadb.wsgi "$BINDADDR:$BINDPORT"
+echo "Starting web interface - go to http://localhost:$BINDPORT/"
+
+export DJANGO_SETTINGS_MODULE="$SETTINGS"
+gunicorn -w 4 oadb.wsgi -b "$BINDADDR:$BINDPORT"

8fd094f8d38f5f1c77eb45ccf50ad0be4c2ade98
Daniel Davis <dan@danizen.net>
2017-08-19 00:28:54 -0400

    Begin work on renderers

--- oadb/oadb/renderers.py
+++ oadb/oadb/renderers.py
@@ -1,6 +1,22 @@
-from rest_framework.renderers import AdminRenderer
+from rest_framework.renderers import BaseRenderer
 
 
-class AdapterAdminRenderer(AdminRenderer):
-    template = 'oadb/admin.html'
+class FastaRenderer(renderers.BaseRenderer):
+    media_type = 'application/x-fasta'
+    format = 'fna'
+    charset = 'utf-8'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        # what to do with this "data" - probably a JSON array
+        return data
+
+
+class CSVRenderer(renderers.BaseRenderer):
+    media_type = 'text/csv'
+    format = 'csv'
+    charset = 'utf-8'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        # what to do with this "data" - probably a JSON array
+        return data
 

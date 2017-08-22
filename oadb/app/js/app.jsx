9ce0f3fc5065e3abf6e72bb7db7fc18fd1e13091
Dan Davis <daniel.davis@nih.gov>
2017-08-22 17:39:56 -0400

    Routing so that React uses different components
    - Install react-router and history
    - Create a browser history
    - Declare a router using browser history
    - Use a different component for 3 different routes

--- oadb/app/js/app.jsx
+++ oadb/app/js/app.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
 
-export default class App extends React.Component {
+const history = createBrowserHistory();
+
+class KitList extends React.Component {
     render() {
-        return <h3>React admin application</h3>;
+        return <h3>List of kits</h3>
+    }
+}
+
+class AdapterList extends React.Component {
+    render() {
+        return <h3>List of adapters</h3>
+    }
+}
+
+class RunList extends React.Component {
+    render() {
+        return <h3>List of runs</h3>
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router history={history}>
+                <div>
+                    <Route path='/admin/kit' component={KitList} />
+                    <Route path='/admin/adapter' component={AdapterList} />
+                    <Route path='/admin/run' component={RunList} />
+                </div>
+            </Router>
+        )
     };
 }
+
+export default App;
